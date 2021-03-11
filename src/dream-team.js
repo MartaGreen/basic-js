4da9932ac47583bc90aaf79d0d07f78f805b8461
martagreen <portnykhmaria@gmail.com>
2021-03-11 21:24:02 +0400

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  members = members.sort();
+  if (!Array.isArray(members)) return false
   dream_team_name = "";
   for (let i in members) {
-    if (typeof members[i] == "string") dream_team_name += members[i][0];
+    if (typeof members[i] == "string") dream_team_name += members[i].split(" ").join('')[0];
   };
-  return dream_team_name;
+  return dream_team_name.toUpperCase().split('').sort().join('');
 };
